449cfec47ef62a7ab09140fadf50aae357b51b25
Agustin-Monti <153923960+Agustin-Monti@users.noreply.github.com>
2025-08-21 14:03:30 -0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: ['eunsyrxioxiesiwlxysy.supabase.co'], // ✅ Agregado el dominio de Supabase
+    },
+};
+
+module.exports = nextConfig;
